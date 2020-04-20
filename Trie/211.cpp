03a3423bd518848cf3234ea45a0b8856501eb09b
Hui Xu <huixuh@amazon.com>
2020-04-19 23:00:49 -0700

    211. Add and Search Word - Data structure design
    TRIE TREE

---
+++ Trie/211.cpp
@@ -0,0 +1,27 @@
+#Design a data structure that supports the following operations:
+# void seach(string s)
+# void addString(string s)
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
